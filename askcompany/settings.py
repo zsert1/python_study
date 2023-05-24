b0318747a609a0f3c947e27aa4880f0109c3d640
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-05-24 22:32:00 +0900

    add 3project

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog1'
+    'blog1',
+    'instagram',
 ]
 
 MIDDLEWARE = [
