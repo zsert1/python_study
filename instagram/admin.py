439bfbe724b1d4e6d66f74fef9d587a6a0757392
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-05-24 23:40:17 +0900

    add 4project

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+# # Register your models here.
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(Post,PostAdmin)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id','message','message_lenght','create_at','update_at']
+    list_display_links=['message']
+    
+    # def message_lenght(self,post):
+    #     return len(post.message)
+    
+    def message_lenght(self,post):
+        return f"{len(post.message)} 글자"
+    
--- instagram/models.py
+++ instagram/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     message=models.TextField()
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
+    def message_lenght(self):
+        return len(self.message)
+    message_lenght.short_description="메세지 글자수"
