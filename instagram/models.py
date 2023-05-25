f4fbfdbf9ec299e75dbbb90f4ad89928ca34d466
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-05-26 01:37:27 +0900

    media_settimg

--- instagram/models.py
+++ instagram/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message=models.TextField()
+    photo=models.ImageField(blank=True)
     is_public=models.BooleanField(default=False,verbose_name='공개여부')
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
