8338e7887e4afab8bb18329a75865e694b59e22b
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-05-26 01:58:01 +0900

    media_upload

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Post
-
+from django.utils.safestring import mark_safe
 # admin.site.register(Post)
 # # Register your models here.
 
@@ -10,14 +10,18 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display=['id','message','message_lenght','create_at','update_at','is_public']
+    list_display=['id','photo_tag','message','message_lenght','create_at','update_at','is_public']
     list_display_links=['message']
     search_fields=['message']
     list_filter=['create_at','is_public']
     # search_fields=['name']
     # def message_lenght(self,post):
     #     return len(post.message)
-    
+    def photo_tag(self,post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}"/>')
+        return None
+
     def message_lenght(self,post):
         return f"{len(post.message)} 글자"
     
