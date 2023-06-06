040385f30833641884764cc549081d96c341613c
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-06-06 20:04:01 +0900

    view

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from. import views
-urlpatterns=[
-    path('',views.post_list),]
+from . import views
+urlpatterns = [
+    path('', views.post_list),
+    path('<int:pk>/', views.post_detail)]
--- instagram/views.py
+++ instagram/views.py
@@ -1,15 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q','')
-    if q:
-        qs= qs.filter(message__icontains=q)
-    #instagram/templates/instagram/post_list.html
-    return render(request,'instagram/post_list.html',{
-        "post_list":qs,
-        'q':q,})
-    # request.POST
-    # request.FILES
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         "post_list": qs,
+#         'q': q, })
+# request.POST
+# request.FILES
 # Create your views here.
+
+
+def post_detail(request, pk):
+    reponse = HttpResponse()
+    reponse.write('helloWord')
+    return reponse
