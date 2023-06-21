aa727776b6feea2579bf801516e5a9b4c43e389b
DESKTOP-QTC34K2\AA <zsert1@naver.com>
2023-06-21 23:42:07 +0900

    classview

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
-
+from django.views.generic import ListView,DetailView
 post_list = ListView.as_view(model=Post)
 
 # def post_list(request):
@@ -19,10 +18,22 @@ post_list = ListView.as_view(model=Post)
 # Create your views here.
 
 
-def post_detail(request, pk):
-    reponse = HttpResponse()
-    reponse.write('helloWord')
-    return reponse
+# def post_detail(request, pk):
+#     reponse = HttpResponse()
+#     reponse.write('helloWord')
+#     return reponse
 
+    # reponse = HttpResponse()
+    # reponse.write('helloWord')
+    # return reponse
+# def post_detail(request:HttpResponse, pk:int)->HttpResponse:
+#     try:
+#       post=Post.objects.get(pk=pk)
+#     except Post.DoesNotExist:
+#       raise Http404
+#     return render(request,'instagram/post_detail.html',{'post':post})
+post_detail=DetailView.as_view(model=Post)
 def archives_years(request,year):
     return HttpResponse(f"{year}년 archive")
+
+
